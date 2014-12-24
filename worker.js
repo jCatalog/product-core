ffd9c790e47632908b1512bec9f886f22d64ddc6
Ezequiel Morito <mail@ezequielmorito.com.ar>
2014-12-24 14:11:21 -0500

    insert by one

--- worker.js
+++ worker.js
@@ -18,121 +18,32 @@ jobs.process('perf.tests', function(job, done){
   console.log('Started job ' + job.data.name);
 
   for (var x = 1; x <= 1000; x++) {
-    async.series([
-        function(callback){
-            Product.create({
-              tenantId: x,
-              supplierId: '1',
-              statusId: '1',
-              mfgProductId: '1',
-              mfgProductName: 'Test',
-              manufactererId: '1',
-              manufactererName: 'Test',
-              extProductId: '1',
-              productIdExtension: '1',
-              unitOfMeasureId: '1',
-              salesUnitOfMeasureId: '1',
-              keywords: 'key-1, key-2',
-              ean: '123456',
-              isMainProdLine: true,
-              isForSales: true,
-              isSpecialOffer: true,
-              isStocked: true,
-              isPunchout: true,
-              isConfigurable: true,
-              validFrom: '2014-12-05',
-              validTo: '2015-06-01',
-              createdBy: 'foo@bar'
-            });
-            console.log('  - Inserted-1');
-            callback(null);
-        },
-        function(callback){
-            Product.create({
-              tenantId: x,
-              supplierId: '1',
-              statusId: '1',
-              mfgProductId: '1',
-              mfgProductName: 'Test',
-              manufactererId: '1',
-              manufactererName: 'Test',
-              extProductId: '1',
-              productIdExtension: '1',
-              unitOfMeasureId: '1',
-              salesUnitOfMeasureId: '1',
-              keywords: 'key-1, key-2',
-              ean: '123456',
-              isMainProdLine: true,
-              isForSales: true,
-              isSpecialOffer: true,
-              isStocked: true,
-              isPunchout: true,
-              isConfigurable: true,
-              validFrom: '2014-12-05',
-              validTo: '2015-06-01',
-              createdBy: 'foo@bar'
-            });
-            console.log('  - Inserted-2');
-            callback(null);
-        },
-        function(callback){
-            Product.create({
-              tenantId: x,
-              supplierId: '1',
-              statusId: '1',
-              mfgProductId: '1',
-              mfgProductName: 'Test',
-              manufactererId: '1',
-              manufactererName: 'Test',
-              extProductId: '1',
-              productIdExtension: '1',
-              unitOfMeasureId: '1',
-              salesUnitOfMeasureId: '1',
-              keywords: 'key-1, key-2',
-              ean: '123456',
-              isMainProdLine: true,
-              isForSales: true,
-              isSpecialOffer: true,
-              isStocked: true,
-              isPunchout: true,
-              isConfigurable: true,
-              validFrom: '2014-12-05',
-              validTo: '2015-06-01',
-              createdBy: 'foo@bar'
-            });
-            console.log('  - Inserted-3');
-            callback(null);
-        },
-        function(callback){
-            Product.create({
-              tenantId: x,
-              supplierId: '1',
-              statusId: '1',
-              mfgProductId: '1',
-              mfgProductName: 'Test',
-              manufactererId: '1',
-              manufactererName: 'Test',
-              extProductId: '1',
-              productIdExtension: '1',
-              unitOfMeasureId: '1',
-              salesUnitOfMeasureId: '1',
-              keywords: 'key-1, key-2',
-              ean: '123456',
-              isMainProdLine: true,
-              isForSales: true,
-              isSpecialOffer: true,
-              isStocked: true,
-              isPunchout: true,
-              isConfigurable: true,
-              validFrom: '2014-12-05',
-              validTo: '2015-06-01',
-              createdBy: 'foo@bar'
-            });
-            console.log('  - Inserted-4');
-            callback(null);
-        }
-      ]);  
-    }
-    console.log('Finished job ' + job.data.name);
-    done();
-  });
+    Product.create({
+      tenantId: x,
+      supplierId: '1',
+      statusId: '1',
+      mfgProductId: '1',
+      mfgProductName: 'Test',
+      manufactererId: '1',
+      manufactererName: 'Test',
+      extProductId: '1',
+      productIdExtension: '1',
+      unitOfMeasureId: '1',
+      salesUnitOfMeasureId: '1',
+      keywords: 'key-1, key-2',
+      ean: '123456',
+      isMainProdLine: true,
+      isForSales: true,
+      isSpecialOffer: true,
+      isStocked: true,
+      isPunchout: true,
+      isConfigurable: true,
+      validFrom: '2014-12-05',
+      validTo: '2015-06-01',
+      createdBy: 'foo@bar'
+    });
+    console.log('  - Inserted-'+x);
+  }
+  console.log('Finished job ' + job.data.name);
+  done();
+});
